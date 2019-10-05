172bc2c709151abeb6632f835d274a32ce248f7e
Patrick McKay <patrick.mckay@charter.com>
2019-10-04 18:32:05 -0600

    add old data

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import actionRoutes from '../data/action.json';
+import oldActionRoutes from '../data/oldAction.json';
 import noActionRoutes from '../data/noAction.json';
 
 @Injectable({
@@ -14,6 +15,9 @@ export class DataService {
 
   private populateRoutes() {
     this.combinedRoutes = actionRoutes.map(actionRoute => {
+      let combinedRoute = actionRoute;
+
+      // Matching no-action route
       let matchingRoute = noActionRoutes.find(noActionRoute => {
         return noActionRoute.RouteNumber === actionRoute.RouteNumber && noActionRoute.Name === actionRoute.Name && noActionRoute.MgtRngDist === actionRoute.MgtRngDist && (noActionRoute.GIS_Miles === actionRoute.TxtSegMi || noActionRoute.GIS_Miles === actionRoute.GIS_Miles);
       });
@@ -21,22 +25,38 @@ export class DataService {
         return noActionRoute.RouteNumber === actionRoute.RouteNumber && noActionRoute.Name === actionRoute.Name && noActionRoute.MgtRngDist === actionRoute.MgtRngDist;
       });
       if (matchingRoute) {
-        let combinedRoute = actionRoute;
         combinedRoute["AltAmgt1"] = matchingRoute.AltAmgt1;
         combinedRoute["AltAmgt2"] = matchingRoute.AltAmgt2;
         combinedRoute["AltAmgt3"] = matchingRoute.AltAmgt3;
         combinedRoute["AltAmgtSea"] = matchingRoute.AltAmgtSea;
         combinedRoute["AltAmgtPublic"] = matchingRoute.AltAmgtPublic;
-        return combinedRoute;
       } else {
-        let combinedRoute = actionRoute;
         combinedRoute["AltAmgt1"] = "No data";
         combinedRoute["AltAmgt2"] = "No data";
         combinedRoute["AltAmgt3"] = "No data";
         combinedRoute["AltAmgtSea"] = "No data";
         combinedRoute["AltAmgtPublic"] = "No data";
-        return combinedRoute;
       }
+
+      // Matching old action route
+      let oldMatchingRoute = oldActionRoutes.find(oldActionRoute => {
+        return oldActionRoute.RouteNumber === actionRoute.RouteNumber && oldActionRoute.NAME === actionRoute.Name && oldActionRoute.MgtRngDist === actionRoute.MgtRngDist && oldActionRoute.TxtSegMils === actionRoute.TxtSegMi;
+      });
+      if (oldMatchingRoute) {
+        combinedRoute['TAPsurfTy'] = oldMatchingRoute.TAPsurfTy || "No data";
+        combinedRoute['TAPcmnts'] = oldMatchingRoute.TAPcmnts || "No data";;
+        combinedRoute['CurrMTC'] = oldMatchingRoute.CurrMTC || "No data";;
+        combinedRoute['DesiredMTC'] = oldMatchingRoute.DesiredMTC || "No data";;
+        combinedRoute['County'] = oldMatchingRoute.County || "No data";
+      } else {
+        combinedRoute['TAPsurfTy'] = "No data";
+        combinedRoute['TAPcmnts'] = "No data";;
+        combinedRoute['CurrMTC'] = "No data";;
+        combinedRoute['DesiredMTC'] = "No data";;
+        combinedRoute['County'] = "No data";
+      }
+
+      return combinedRoute;
     });
   }
 
